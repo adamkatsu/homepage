8cc745261cb20444df78cf7c96d8ddc83ff49c44
adamkatsu <adamkatsu369@gmail.com>
2025-04-27 21:56:31 +0700

    feat: vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { ReactLenis, useLenis } from 'lenis/react'
+import { ReactLenis, useLenis } from 'lenis/react';
+import { Analytics } from "@vercel/analytics/react"
 import Hero from './sections/Hero';
 import Navbar from './sections/Navbar';
 import WhatIDo from './sections/WhatIDo';
@@ -13,6 +14,7 @@ function App() {
   })
   return (
     <ReactLenis root>
+      <Analytics />
       <ParticleBackground />
       <Hero />
       <Navbar />
